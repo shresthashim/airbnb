bbcb368b5b75b52c18525ec8d97d68cab85dfbf4
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-29 08:41:03 +0545

    deps : install tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 import React from "react";
 import {Open_Sans} from "next/font/google";
 
 
 export const metadata: Metadata = {
-  title: "Airbnb",
-  description: " Vacation Rentals, Cabins, Beach Houses, Unique Homes & Experiences",
+    title: "Airbnb",
+    description: " Vacation Rentals, Cabins, Beach Houses, Unique Homes & Experiences",
 };
 
-const font = Open_Sans({ subsets: ["latin"] });
+const font = Open_Sans({subsets: ["latin"]});
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={font.className}>{children}</body>
+        </html>
+    );
 }
