fa007bd9b76d75adb82e6e4dbcfe6b4db94d292d
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-30 16:04:41 +0545

    chore : avatar component completed

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,10 +1,10 @@
+'use client'
 import React from 'react';
+import Image from "next/image";
 
 const Avatar = () => {
     return (
-        <div>
-            
-        </div>
+        <Image className='rounded-full' height={'30'} width={'30'} alt={"Avatar"} src={"/images/placeholder.jpg"}/>
     );
 };
 
