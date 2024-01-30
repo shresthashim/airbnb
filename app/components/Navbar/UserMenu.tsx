7070d131b0cde5e223eb614e4ba18e269327ed9e
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-30 16:13:50 +0545

    initialise : menuitems for menu in navbar

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -1,9 +1,15 @@
 'use client'
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {AiOutlineMenu} from "react-icons/ai";
 import Avatar from "@/app/components/Avatar";
+import MenuItems from "@/app/components/Navbar/MenuItems";
 
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+
+    }, []);
     return (
         <div className='relative '>
             <div className='flex flex-row items-center gap-3'>
@@ -13,6 +19,7 @@ const UserMenu = () => {
                     Airbnb Your Home
                 </div>
                 <div onClick={() => {
+                    toggleOpen()
                 }}
                      className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'>
                     <AiOutlineMenu/>
@@ -21,6 +28,16 @@ const UserMenu = () => {
                     </div>
                 </div>
             </div>
+            {isOpen && (
+                <div
+                    className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm '>
+                    <div className='flex flex-col cursor-pointer'>
+                        <>
+                            <MenuItems/>
+                        </>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
