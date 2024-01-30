afa417645fc7bd820125c6447195a3d4a184b545
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-30 16:34:16 +0545

    feat : initialise modal props interface

--- app/components/Modals/Modal.tsx
+++ app/components/Modals/Modal.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel?: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryLabel?: string;
+
+}
+
 const Modal = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
