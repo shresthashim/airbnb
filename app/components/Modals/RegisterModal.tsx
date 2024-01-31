3f40b245e4b1822c4b2bda294219ef622954ffef
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-31 19:01:26 +0545

    deps : install axios and react hook form

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -1,5 +1,10 @@
+'use client'
 import React from 'react';
-
+import axios from 'axios';
+import {AiFillGithub} from "react-icons/ai";
+import {FcGoogle} from "react-icons/fc";
+import {useCallback, useState} from "react";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 const RegisterModal = () => {
     return (
         <div>
